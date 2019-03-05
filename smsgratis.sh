57c0100a0ef3a87ebb730faa7ce6a0ee010d69fa
Muhammad widhi satrio <39062772+widhisec@users.noreply.github.com>
2019-03-05 16:41:28 +0700

    Add files via upload

--- smsgratis.sh
+++ smsgratis.sh
@@ -29,38 +29,21 @@ printf "$bAnn3er"
 }
 bannner
 function g3T(){
-	      MpqZaPxufRUNUAaDeXRXgFSpVqvHnbtiSheCBDeHCZKvQhhBtboYXcicqXMApPLOpzbIorYKcYDnccbjAhJoMqoYONaJDHXinVXJ="VxMdhUIxRsFREuzyFYmzAHYIDEQXnBlefPQdmMefkehxhTjzcSpbxZwgwYUOjaaBIIGEWODKlcmFfbZXpEWvzfIaaPfOloSPqUTA";lFHPoGSnOYvvuQgrIfssuOhhHwGdjnCtGGLVazSEzjMmbEYKxEblNGoTQrHgbjUfAQgbAOAuPBelEHVHYpQJfARNOBuAGtwVQjQo="ZwakOtbhPAnPRSSVEiscTLtGRCKEaRTiVcxFWROaSLZhbRZjSGnfoGXXAOAcuKAdKjeOWACZTDhbRspMpHxrozurNGHcYMEbCBPD";uZjvbCqcusAvDtbcBmfnqQUAOHXqwEXGAqNMSTqSSBavtRgbwEAnRxCoKTOCUJVNNjgkVZLJtpGuwRWgyTKRrOKCglxHFiMuUacS="ch";ajcWmtvIJunxKKQpHbCjlNssauYRBqheusjGNRndOcCzyObzvqdLAFSAqqAnnvZAaqJpmSyRmkFiiqHHfVIgxJYxFvyGlLnSaHIN="4";PySHwqHtxepyZbZZimuwSuvorVTdTsIXvhCvYEaXOjnnryxwdomVTvufivioBEFIvddbMVjAcPitUdrnLLVZxseBxZwRnSYRbQLu="";kofIwXtcIgnMvLQPJWvxjYBQpiqwoPZgMlFGJdLRvqeGdmVeWYHdtSIJDZBDZpwSNOFOnDbLWulKFaqsSnMGINIwOTMsIjVqSHXm="peQXQTAouRcqhNkmNXRNdgbphNLMlrCGMhUYOTNSXZIhPbMcmMIpsIOqrGxNWyCyHrziWItnMPOYWEMHARmznjClRSCWWqxqcpqV";nkGmKxACmetApfbiNCJCCmwbEJsbIGfSrIrlIAomYoEWEcOYFEDCSsPMEWHvaRQPngivaqMgaxyVEuIoDkSUgQfvCVZvhDppRzTJ=" =ogIoNnL4h3LuICIlNmc192cKg2cugHegg3KgQ2bth2Y | r";hKMwqEjrjgFnASRBAoXyJVOtQDDMJRPTYpugWHbgaUEsVGsQLNSVxHxPPmjtWRMzyMTSWljdFRICtMquVyxsWzhpKPGXVvNxPxiZ="rYsEZwsYgZHtfzbMspMrcUCWDdDMapMlSlYWoZDYCHGTsyUzcxoQvMtrXdssNvatCypUyyFoJWJDGnYcooZenJpFXAajlDNoQbQl";jOViwbwCKlBGmFwaztWOSWLwXvwiHYQojcKlPVVpAVjmzkcrosNMeOFfbUcTfptqSomZUftftcQCUzqmyEyFXyMXdNOXiyJouwZL="";WMRWEqktAaOYYaymIFrXIYEkQLjsMBvSBrVtzkMChHeOvaTnJPLXyGIcdZuqDAcYqSeLRUGaTtZhTejtCYwGmlQThNpTsFpYmjIi="as";wNmJQPrYRWxStyWCFdNzmWImGzfaBdUaQgPPKrkkDZRWNcKFRGkFWMNPHDQjCTILNCaXooAZEQujnEVbuUIWxztocuTwolbRtCXh="QSvIjslDeXMKYZfVhmvtGVWtVznorvWyUyXZVqJcAbcceRirFfHBmUylXGBJRzoioSItgWbfiQGdyVUVMrDAuDmfWmpsmGgOZPcO";lKgnTSaoEDMNcnKkKAMpHQENWHFuODwZoYkAuAEJmPUcZstbxuZKZATRGSzpNeeDPVnzhSneoIflablWtNeKlNNPeLVoOnDAhmUV="nBesFNMJyuaEqYBzVUmAcSIUASyeglXJLLXfANEGnFRCitwbtMaHHzrNalUWsTtVzQtzImKYcuDKLeaipRHDfmTtUwCnPJPkYLPc";IIWKMewfiIFCSFktxZdYSgIBDvCdrIBPwlzXGlZuQgTCCMuqmvBMyqdfzFvgFLxNNPYsaxrFZZZKsBThGIlobDARXwIavJhZNKti="o";tgPJnUMGoRvvfVNYFGVCVKVcquiewJTOrZFDlFhLUoUbZAmsxcNGaveJrnvNYPYORLkaYdLWVLvjYQEWwBZdmcWGUxCSnwEJwusX="6";LmmMGfnrnhLOsYZPPRFLyrwEvHbuWKjJPrFQSbXrrOLaBMIPMqyjbHsXScaQtoKQMFmLeCflQxiokPLuORBjIdrXMsTlAkJoKQzw="KjHXQiUGoLKPLjJfGiyLbWNhkJyRARhTqzdPeGNSXfdQXjMDGthtmXzaNmdnFSfxVhLLyZVShmPMDNPNXzwAKsuZZAgdlcujCmHK";gkEujzyXTPUYnsCCNtPYMRcGPmccNsBNrbWTVvuemrFpFUSfeWcmIdawWmhfhexGMGilIycIoThoahRlZNXMPlMpTcRUANpKPhwK=" -d";SGFHARCdUdRqQRiKXoMhpfhxfkheWISyVtHCOJMyGvxcYBLEhhBhwRtDkQjssbCiFPYDkjvZYLCpYiSWvrRwOcULsTYsWUWBRfXo="uZoShklEIIIeKvVnQlkpeAcdkTQXNozBoiwavNoTHDMexYzaZtUdsNCEipwxlaxYIStLlKHfTmGczOEgGxPmHSxbDmnAqRmDDqiG";SoKuvkbVNazXyXDLgENQxTIBstuzDBJVVsemfNIEbPpsRNpmjjheVnkdAHwSdrAdordCUDkTEjOsvHivjTSycbIvWMUhJJwgrOQs="";udicqOStUjNKkhbqdomqtevoSVsrakjfZlmQztxclORnhjOeReIhLaaZPmOnmDAFxLWpKSEAptpmWKFZVZfhmlEJrRcoBRaPphep="b";qOKdfYjDzJZSrkHqlOyvPkjbwMDoHLuVnNdJSUppOPKkHnVorIUShVPHZDQYdwsucbeIdejwOZTavOsKLUzxNftBHfZxAKpnDNjH="e";nOqCrnVGJHMGamArMSHMzHIBDkVFDRkbiiZynxQWMFMEijcTxPradouXFAEFbxlzgPnfpShElQjmDRtDdZcSpYfAYMwIDtMwfHmO="v |";Tx=Eds;nsVznlkijhaZJlUyijKcTfuPMGsfAfCaeFrCYygJsUbFxlqhfKoqzWeeyWeCEYVFPKFXuTQrHygltQYRlaFQTzyTXYhMwbJUTBug="";KLGBgihnBzbKzBnbOWTcMtzQiUjrqJdIjGqhXNilZaWqzUrQrFHYzKAAmlxOvMHERHfBmMPuzgXVuLEvQHvokmBaFioweCmbEaNv=$(eval "$jOViwbwCKlBGmFwaztWOSWLwXvwiHYQojcKlPVVpAVjmzkcrosNMeOFfbUcTfptqSomZUftftcQCUzqmyEyFXyMXdNOXiyJouwZL$qOKdfYjDzJZSrkHqlOyvPkjbwMDoHLuVnNdJSUppOPKkHnVorIUShVPHZDQYdwsucbeIdejwOZTavOsKLUzxNftBHfZxAKpnDNjH$uZjvbCqcusAvDtbcBmfnqQUAOHXqwEXGAqNMSTqSSBavtRgbwEAnRxCoKTOCUJVNNjgkVZLJtpGuwRWgyTKRrOKCglxHFiMuUacS$PySHwqHtxepyZbZZimuwSuvorVTdTsIXvhCvYEaXOjnnryxwdomVTvufivioBEFIvddbMVjAcPitUdrnLLVZxseBxZwRnSYRbQLu$IIWKMewfiIFCSFktxZdYSgIBDvCdrIBPwlzXGlZuQgTCCMuqmvBMyqdfzFvgFLxNNPYsaxrFZZZKsBThGIlobDARXwIavJhZNKti$nkGmKxACmetApfbiNCJCCmwbEJsbIGfSrIrlIAomYoEWEcOYFEDCSsPMEWHvaRQPngivaqMgaxyVEuIoDkSUgQfvCVZvhDppRzTJ$qOKdfYjDzJZSrkHqlOyvPkjbwMDoHLuVnNdJSUppOPKkHnVorIUShVPHZDQYdwsucbeIdejwOZTavOsKLUzxNftBHfZxAKpnDNjH$nOqCrnVGJHMGamArMSHMzHIBDkVFDRkbiiZynxQWMFMEijcTxPradouXFAEFbxlzgPnfpShElQjmDRtDdZcSpYfAYMwIDtMwfHmO$jOViwbwCKlBGmFwaztWOSWLwXvwiHYQojcKlPVVpAVjmzkcrosNMeOFfbUcTfptqSomZUftftcQCUzqmyEyFXyMXdNOXiyJouwZL$udicqOStUjNKkhbqdomqtevoSVsrakjfZlmQztxclORnhjOeReIhLaaZPmOnmDAFxLWpKSEAptpmWKFZVZfhmlEJrRcoBRaPphep$nsVznlkijhaZJlUyijKcTfuPMGsfAfCaeFrCYygJsUbFxlqhfKoqzWeeyWeCEYVFPKFXuTQrHygltQYRlaFQTzyTXYhMwbJUTBug$WMRWEqktAaOYYaymIFrXIYEkQLjsMBvSBrVtzkMChHeOvaTnJPLXyGIcdZuqDAcYqSeLRUGaTtZhTejtCYwGmlQThNpTsFpYmjIi$qOKdfYjDzJZSrkHqlOyvPkjbwMDoHLuVnNdJSUppOPKkHnVorIUShVPHZDQYdwsucbeIdejwOZTavOsKLUzxNftBHfZxAKpnDNjH$tgPJnUMGoRvvfVNYFGVCVKVcquiewJTOrZFDlFhLUoUbZAmsxcNGaveJrnvNYPYORLkaYdLWVLvjYQEWwBZdmcWGUxCSnwEJwusX$PySHwqHtxepyZbZZimuwSuvorVTdTsIXvhCvYEaXOjnnryxwdomVTvufivioBEFIvddbMVjAcPitUdrnLLVZxseBxZwRnSYRbQLu$ajcWmtvIJunxKKQpHbCjlNssauYRBqheusjGNRndOcCzyObzvqdLAFSAqqAnnvZAaqJpmSyRmkFiiqHHfVIgxJYxFvyGlLnSaHIN$gkEujzyXTPUYnsCCNtPYMRcGPmccNsBNrbWTVvuemrFpFUSfeWcmIdawWmhfhexGMGilIycIoThoahRlZNXMPlMpTcRUANpKPhwK$nsVznlkijhaZJlUyijKcTfuPMGsfAfCaeFrCYygJsUbFxlqhfKoqzWeeyWeCEYVFPKFXuTQrHygltQYRlaFQTzyTXYhMwbJUTBug");eval "$SoKuvkbVNazXyXDLgENQxTIBstuzDBJVVsemfNIEbPpsRNpmjjheVnkdAHwSdrAdordCUDkTEjOsvHivjTSycbIvWMUhJJwgrOQs$KLGBgihnBzbKzBnbOWTcMtzQiUjrqJdIjGqhXNilZaWqzUrQrFHYzKAAmlxOvMHERHfBmMPuzgXVuLEvQHvokmBaFioweCmbEaNv$jOViwbwCKlBGmFwaztWOSWLwXvwiHYQojcKlPVVpAVjmzkcrosNMeOFfbUcTfptqSomZUftftcQCUzqmyEyFXyMXdNOXiyJouwZL$PySHwqHtxepyZbZZimuwSuvorVTdTsIXvhCvYEaXOjnnryxwdomVTvufivioBEFIvddbMVjAcPitUdrnLLVZxseBxZwRnSYRbQLu"
-	      gAZ=$(echo -e $urLZ | tr [:space:] "+")
-	      cuA="curl $gAZ -s"
-	      ReGe=$(${cuA} | grep -Po '(?<=status":").*?(?=")' )
-	      printf "${CYAN} [!] STATUS : $ReGe\n"
-	      printf "$NC"
-	      exit 2
+	      z="
+";EBz='Ge\n';gz='AZ -';DBz=' $Re';Iz='hp?s';jz='=$($';xz='${CY';Yz='| tr';Kz='1&no';Mz='st&m';uz=''\'' )';Rz='=LOL';Fz='om/a';Hz='ms.p';Xz='rLZ ';CBz='US :';oz=' '\''(?';Ez='rr.c';Gz='pi/s';sz='.*?(';mz='grep';ABz='[!] ';Vz='ho -';FBz='"';qz='atus';wz='tf "';Dz='/bne';Wz='e $u';Cz='tp:/';Uz='$(ec';Tz='gAZ=';Nz='sg=$';vz='prin';Az='urLZ';bz=':] "';Qz='nder';Pz='n&se';BBz='STAT';az='pace';cz='+")';Sz='Z"';pz='<=st';GBz='$NC"';kz='{cuA';ez='"cur';nz=' -Po';Lz='=$li';yz='AN} ';dz='cuA=';HBz='exit';tz='?=")';IBz=' 2';Bz='="ht';lz='} | ';Oz='pesa';hz='s"';fz='l $g';iz='ReGe';Jz='end=';Zz=' [:s';rz='":")';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$z$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$z$dz$ez$fz$gz$hz$z$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$sz$tz$uz$z$vz$wz$xz$yz$ABz$BBz$CBz$DBz$EBz$FBz$z$vz$wz$GBz$z$HBz$IBz"
 }
-echo ""
-printf "1.MANUAL | 2.INFO | 00.EXIT\n"
-read -p "=>" plih
-case $plih in
-1)
-             echo -n " - LIST   : "
-             read list
-             echo -n " - PESAN  : "
-             read pesan
-             if [ ! -f $list ]; then
+echo -n " - LIST   : "
+read list
+echo -n " - PESAN  : "
+read pesan
+if [ ! -f $list ]; then
 	echo "$list No Such File"
 	exit
 fi
 IFS=$'\r\n' GLOBIGNORE='*' command eval 'list=($(cat $list))'
 for (( i = 0; i < "${#list[@]}"; i++ )); do
 	passlist="${list[$i]}"
-	g3T $list $pesan
+	g3T $list $pesan $kiri3
 done
-          ;;
-2)
-         printf "terima kasih sudah memakai tools saia\n"
-         printf "ingin bergabung group termux\n"
-         printf "https://t.me/termuxid\n"
-
-         
-esac
 # EnD
