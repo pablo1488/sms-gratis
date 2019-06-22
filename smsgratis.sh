8e7944f3bfcc8c1c79fdce08f8b2b656829c3d37
hax0r <39062772+widhisec@users.noreply.github.com>
2019-06-22 14:20:45 +0700

    Add files via upload

--- smsgratis.sh
+++ smsgratis.sh
@@ -42,7 +42,17 @@ ${CYAN} /    /  \___ \\  ___/\  \__\  \___\  \_/   \/ /_/ \  ___/ - ${CYAN}CREW.
 
 }
 function negetgan(){
-	   okeey1=$(curl -s 'http://sms.payuterus.biz/alpha/index.php?a=keluar' \
+         bypass=$(curl -s 'http://sms.payuterus.biz/alpha/index.php?a=keluar' \
+           -H 'Connection: keep-alive' \
+           -H 'Pragma: no-cache' \
+           -H 'Cache-Control: no-cache' \
+           -H 'Upgrade-Insecure-Requests: 1' \
+           -H 'User-Agent: Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' \
+           -H 'Referer: http://sms.payuterus.biz/alpha/send.php' \
+           -H 'Accept-Encoding: gzip, deflate' \
+           -H 'Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7' \
+           -H 'Cookie: _ga=GA1.2.131924726.1560439960; PHPSESSID=jjrqqaakmfcgfgbtjt8tve5595; _gid=GA1.2.1969561921.1561024035; _gat=1' --compressed --insecure | grep "value" | grep "name" | grep -oP 'value="\K[^"]+')
+	       okeey1=$(curl -s 'http://sms.payuterus.biz/alpha/index.php?a=keluar' \
            -H 'Connection: keep-alive' \
            -H 'Pragma: no-cache' \
            -H 'Cache-Control: no-cache' \
@@ -74,7 +84,7 @@ function negetgan(){
          -H 'Referer: http://sms.payuterus.biz/alpha/' \
          -H 'Accept-Encoding: gzip, deflate' \
          -H 'Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7' \
-         -H 'Cookie: _ga=GA1.2.131924726.1560439960; PHPSESSID=jjrqqaakmfcgfgbtjt8tve5595; _gid=GA1.2.1969561921.1561024035' --data "nohp=$now&pesan=$psn&captcha=$ok" --compressed --insecure | grep -o 'value="[^"]*"')
+         -H 'Cookie: _ga=GA1.2.131924726.1560439960; PHPSESSID=jjrqqaakmfcgfgbtjt8tve5595; _gid=GA1.2.1969561921.1561024035' --data "nohp=$now&pesan=$psn&captcha=$ok&key=$bypass" --compressed --insecure | grep -o 'value="[^"]*"')
          if [[ $okeey =~ "SMS Gratis Telah Dikirim" ]]; then
              echo -e "${GREEN}[+] ${NORMAL}mantap berhasil kirim gayn"
          else
